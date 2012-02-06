b9c23e6af506617541dcf8018548574e88f7d501
Aram Balakjian <aram@aabweb.co.uk>
2012-02-06 13:35:42 +0000

    Fixed delete from table list bug
    Signed-off-by: Aram Balakjian <aram@aabweb.co.uk>

--- code/ModelAdmin/DataObjectAsPageAdmin.php
+++ code/ModelAdmin/DataObjectAsPageAdmin.php
@@ -3,6 +3,7 @@
 class DataObjectAsPageAdmin extends ModelAdmin 
 {
 	public static $record_controller_class = "DataObjectAsPageAdmin_RecordController";
+	protected $resultsTableClassName = 'DataObjectAsPageTableField';
 	
 	public function init() 
 	{
@@ -18,9 +19,14 @@ class DataObjectAsPageAdmin_RecordController extends ModelAdmin_RecordController
 {	
 	public function doPublish($data, $form, $request)
 	{
-		$form->saveInto($this->currentRecord);
+		$record = $this->currentRecord;
+		
+		if($record && !$record->canPublish())
+		        return Security::permissionFailure($this);	
+			
+		$form->saveInto($record);
 
-		$this->currentRecord->doPublish();
+		$record->doPublish();
 
         if(Director::is_ajax()) {
            	return $this->edit($request);
@@ -31,9 +37,12 @@ class DataObjectAsPageAdmin_RecordController extends ModelAdmin_RecordController
 	
 	public function doUnpublish($data, $form, $request)
 	{
-		$form->saveInto($this->currentRecord);
+		$record = $this->currentRecord;
 		
-		$this->currentRecord->doUnpublish();
+		if($record && !$record->canDeleteFromLive())
+		        return Security::permissionFailure($this);
+
+		$record->doUnpublish();
 		
         if(Director::is_ajax()) {
            	return $this->edit($request);
@@ -44,7 +53,12 @@ class DataObjectAsPageAdmin_RecordController extends ModelAdmin_RecordController
 	
 	public function doDelete($data, $form, $request)
 	{
-		$this->currentRecord->doDelete();
+		$record = $this->currentRecord;
+		
+		if($record && !$record->canDelete())
+        	return Security::permissionFailure();
+		
+		$record->doDelete();
 		
         if(Director::is_ajax()) {
            	$this->edit($request);
@@ -54,7 +68,7 @@ class DataObjectAsPageAdmin_RecordController extends ModelAdmin_RecordController
 	}	
 	
 	public function duplicate($data, $form, $request) { 
-         
+        
         //Duplicate the object
         $Clone = $this->currentRecord->duplicate();
 
