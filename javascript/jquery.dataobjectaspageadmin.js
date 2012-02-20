60afb486f316b20711ad8c770ffb89f920ee01e1
Marcus Dalgren <marcus.dalgren@overlay.se>
2012-02-20 18:09:36 +0100

    Fixing js for list view load and adding a template

--- javascript/jquery.dataobjectaspageadmin.js
+++ javascript/jquery.dataobjectaspageadmin.js
@@ -1,19 +1,17 @@
-
-
 (function($) {
 $(document).ready(function() {   
-   var doList = function() {
-     var currentModel = $('#ModelClassSelector').children('select');
-     var currentModelName = $('option:selected', currentModel).val();
-     var strFormname = "#Form_SearchForm" + currentModelName.replace('Form','');
-     $(strFormname).submit();
-     return false;
-   }
+	var doList = function() {
+		var currentModel = $('#ModelClassSelector').children('select');
+		var currentModelName = $('#SearchForm_holder .tabstrip a').text();
+		var strFormname = "#Form_SearchForm_" + currentModelName;
+		$(strFormname).submit();
+		return false;
+	}
    
    $('#ModelClassSelector').live("change",doList);
    $('#list_view').live("click",doList);
    if($('#list_view_loading').length) {
-     doList();
+		doList();
    }
    $('button[name=action_clearsearch]').click(doList);
 
@@ -48,5 +46,4 @@ $(document).ready(function() {
 	});
 	
 });
-
 })(jQuery);
